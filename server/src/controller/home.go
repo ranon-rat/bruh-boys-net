66b3be8c214bbe036e4a205e845da05f71c600bf
LeonardSx <MatheoJE_2000@hotmail.com>
2023-02-21 22:33:21 -0500

    no phone

--- server/src/controller/home.go
+++ server/src/controller/home.go
@@ -4,12 +4,17 @@ import (
 	"fmt"
 	"net/http"
 	"os"
+	"strings"
 	"text/template"
 )
 
 func HomeTemplate(w http.ResponseWriter, r *http.Request) { // later i will improve the templates for now its ready
 	// i dont really want to use javascript outside of making some art or something but I will not use javascript for anything else
-
+	agent := r.Header.Get("User-Agent")
+	if strings.Contains(agent, "Mobile") {
+		fmt.Fprintf(w, "<html><body><h1>no phone fags allowed</h1></body></html>")
+		return
+	}
 	file, _ := os.ReadFile("templates/index.html")
 	tmpl := template.Must(template.New("a").Parse(string(file)))
 	if err := tmpl.Execute(w, postAvaible); err != nil {
