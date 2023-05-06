f2e1398d957fdf670543b41ae0b3d7033cd509b9
ranon rat <66473662+ranon-rat@users.noreply.github.com>
2023-05-06 00:53:54 -0600

    Update animation.ts

--- server/static/ts/animation.ts
+++ server/static/ts/animation.ts
@@ -1,5 +1,8 @@
+//canvas stuff
 const canvas = document.getElementById("title") as HTMLCanvasElement;
-const howMany = 10
+const ctx = canvas.getContext("2d")!;
+// text animation
+const howMany = 10//how many random phrases
 const randomPhrases = ["The CIA is after me",
   "Accept your malice",
   "The industrial society and its consequences have been a disaster for the human race",
@@ -8,8 +11,10 @@ const randomPhrases = ["The CIA is after me",
   "brave new world", "im not even human anymore", "cryptography", "they have access to my network", "we worship a different god", "I have become death, the destroyer of worlds",
   "they controll us",
   "psyop psyop psyop psyop", "i hate cities", "im in a watchlist", "the key for god is mathematics", "you will own nothing and you will be happy", "👁", "👁", "👁", "👁", "👁", "👁", "👁", "👁", "👁", "👁", "👁", "👁"];
-const howManyImages = 29
-const ctx = canvas.getContext("2d")!;
+// background animation
+  const howManyImages = 29// this is for knowing the random images
+  let change = false;
+
 // background and canvas style setted in here
 canvas.height = canvas.clientHeight;
 canvas.width = canvas.clientWidth;
@@ -17,7 +22,6 @@ canvas.style.backgroundPosition = "center"
 canvas.style.backgroundAttachment = "fixed"
 canvas.style.backgroundImage = 'url("static/images/television-static.gif")'
 
-let change = false;
 
 window.addEventListener("resize", () => {
   canvas.height = canvas.clientHeight;
@@ -60,10 +64,10 @@ function animation(ctx: CanvasRenderingContext2D, canvas: HTMLCanvasElement) {
   // i cant just put the font size without the font so i set this
   // i dont really care but you need to take that in mind
   ctx.font = "10vw Star Wars";
-  drawText(canvas.width / 2, canvas.height / 2, move, "👁 Tecnopsicosis 👁");
+  drawText(canvas.width / 2, canvas.height / 2, move, "👁 Tecnopsychosis 👁");
 // this is for keeping something constant
   ctx.fillStyle = "rgb(255,0,0)"
-  ctx.fillText("👁 Tecnopsicosis 👁", canvas.width / 2, canvas.height / 2)
+  ctx.fillText("👁 Tecnopsychosis 👁", canvas.width / 2, canvas.height / 2)
 }
 setInterval(async () => {
   // i need to clear everything
